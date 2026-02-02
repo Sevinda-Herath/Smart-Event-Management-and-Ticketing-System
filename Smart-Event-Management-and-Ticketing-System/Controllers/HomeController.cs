9bb01c55639921813dba44fa723f45b8d99e88b6
Sevinda Herath <Sevinda Herath@DESKTOP-QK51FT0>
2026-02-02 11:47:51 +0530

    Add project files.

---
+++ Smart-Event-Management-and-Ticketing-System/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Smart_Event_Management_and_Ticketing_System.Models;
+
+namespace Smart_Event_Management_and_Ticketing_System.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
